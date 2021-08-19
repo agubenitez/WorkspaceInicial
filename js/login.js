bd9ab869a9b62653f5f66769bb43e0b572373b2b
agubenitez <69943978+agubenitez@users.noreply.github.com>
2021-08-19 18:08:37 -0300

    entrega 1 completa, se agrego el login y la lista de productos

--- js/login.js
+++ js/login.js
@@ -1,6 +1,31 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+
+document.addEventListener("DOMContentLoaded", function() {
+    //con este evento hago que cuando se haga click en el boton submit del formulario con id "formulario" se ejecute la funcion validarFormulario
+       document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  // esta funcion valida que no haya campos vacios tanto en usuario como en contraseña, y si los hay se muestra un alert y un contorno rojo en el area de texto
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    //obtengo los input de usuario y contraseña mediante su id
+    var usuario = document.getElementById('login');
+    var clave = document.getElementById('password');
+      
+    // si el largo del valor del input es 0, significa que el campo esta vacio y en ese caso muestro el alert y el borde rojo
+    if(usuario.value.length == 0) {
+      alert('No has escrito nada en el usuario');
+      usuario.style.borderColor="red"
+      return;
+    }
+    if (clave.value.length == 0) {
+      alert('No has escrito nada en la contraseña');
+      clave.style.borderColor="red"
+      return;
+    }
+    //si no entra a ningun if, se ejecuta el submit del formulario que me va a llevar a home.html
+    this.submit();
+  }
--- js/products.js
+++ js/products.js
@@ -3,13 +3,15 @@
 //elementos HTML presentes.
 
 
-
+//Funcion que muestra la lista de productos
 function showProductsList(productsArray){
-
+    //Declaro la variable que va a contener el codigo html con la lista de productos
     let htmlContentToAppend = "";
+    //Recorro el array que que contendra una lista de objetos en la que cada objeto corresponde a un producto.
     for(let i = 0; i < productsArray.length; i++){
+        //declaro una variable que corresponde al item de la lista por el que esta pasando el for, esta correspondera a un producto
         let products = productsArray[i];
-
+        //agrego a la variable que contiene el html los datos del producto 
            htmlContentToAppend +=`
            <a href="" class="list-group-item list-group-item-action">
                <div class="row">
@@ -31,11 +33,15 @@ function showProductsList(productsArray){
            `
         }
 
-        document.getElementById("container-products-list").innerHTML = htmlContentToAppend;
+       //mediante el metodo innerHTML agrego a mi HTML el contenido de la variable htmlContentToAppend que contiene la lista de productos generada en el for anterior
+       document.getElementById("container-products-list").innerHTML = htmlContentToAppend;
     }
-   
+      //el evento DOMContentLoaded, se ejecuta cuando se carga la pagina
     document.addEventListener("DOMContentLoaded", function(e){
+        //le psao a la funcion getJSONData que va a generar un objeto de tipo JSON la URL correspondiente a la lista productos
+        //el objeto de tipo JSON se guarda en resultObj
         getJSONData(PRODUCTS_URL).then(function(resultObj){
+            //si los datos se cargaron correctamente ejecuto la funcion showProductsList que va a mostrar la lista de productos
          if (resultObj.status === "ok"){
             showProductsList( resultObj.data);
         }
