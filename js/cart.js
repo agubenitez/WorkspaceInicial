c6e2dbdde79c9d9063cb7c592baea5166112bda1
agubenitez <69943978+agubenitez@users.noreply.github.com>
2021-10-14 13:05:40 -0300

    m

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,25 @@
 var currentCart =[];
 const VALOR_DOLAR  = 44
 var currentSubTotal = 0
+var subTotales = []
 var total = 0;
+var total1 = 0;
+var moneda = "UYU";
+
+
 function showCart(){     
   var cartListId = document.getElementById("cartListId")
-  var totalDiv = document.getElementById("totalDiv")
-  var articles =  currentCart.articles
-
-  var count = 0
- for (let i = 0; i <articles.length; i++) {
+   var articles =  currentCart.articles
+   for (let i = 0; i <articles.length; i++) {
    let CartProduct = articles[i];
 
    //CONVERSIONES 
-   cost = CartProduct.unitCost
-   if(CartProduct.currency=="USD"){
-       cost = cost*VALOR_DOLAR
-   }
+  //  costo = CartProduct.unitCost
+  //  if(CartProduct.currency=="USD"){
+  //      cost = cost*VALOR_DOLAR
+  //  }
+
+  cost = convertir(CartProduct.unitCost, CartProduct.currency)
   cartListId.innerHTML += `
    <li class="cartList">
    <!-- IMAGEN -->
@@ -36,37 +40,63 @@ function showCart(){
   <input id="`+i+`" onInput="subtotal(`+i+`,`+cost+`)" type="number" placeholder="1" step="1" min="0" max="99" value=`+CartProduct.count+`>
   <br>
   <strong  id="`+i+`Sub" >
-  Subtotal: UYU `+CartProduct.count*cost+` 
+  Subtotal: `+moneda+` `+CartProduct.count*cost+` 
     </strong>
  </div>
  </li>     
-
      `
 
- 
+ subTotales[i] = CartProduct.count*cost
+
 
  }
 
-totalDiv.innerHTML = `<strong style="float:right">
-TOTAL: UYU `+total+`
-</strong>   `
+ sumarTotal()
+// totalDiv.innerHTML = `<strong style="float:right">
+// TOTAL: UYU `+total+`
+// </strong>   `
 
 }
 
 function subtotal(i,cost){
   count = document.getElementById(i).value
+  currentSubTotal = count*cost
 
- currentSubTotal = count*cost
   sub = document.getElementById(i+"Sub")
   sub.innerHTML =`  
-  Subtotal: UYU `+currentSubTotal+`
+  Subtotal: `+moneda+` `+currentSubTotal+`
   `
-  subValue.value = currentSubTotal
-  total += currentSubTotal
-  console.log(total)
+  subTotales[i]=currentSubTotal
+  console.log(subTotales)
+  sumarTotal();
 }
 
- 
+function sumarTotal(){
+  var totalDiv = document.getElementById("totalDiv")
+  var total = 0
+  for (let i = 0; i <subTotales.length; i++) {
+   total += subTotales[i]
+   
+  }
+  totalDiv.innerHTML = `<strong style="float:right">
+  TOTAL: `+moneda+` `+total+`
+  </strong>   `
+  
+}
+function convertir(costo, currency){
+   if (moneda == "UYU" && currency == "USD"){
+   costo = costo*40
+ }
+ if (moneda == "USD" && currency == "UYU"){
+  costo = costo/40
+}
+//  else (moneda == "USD" && currency =="UYU")
+//  {
+//    costo = costo/40
+//  }
+ return costo
+ }
+
 
 
 
