6431b76c067cf39a5ca5e41a2720920e9355cc98
agubenitez <69943978+agubenitez@users.noreply.github.com>
2021-11-14 15:33:00 -0300

    comentarios

--- js/cart.js
+++ js/cart.js
@@ -5,9 +5,48 @@ var subTotales = []
 var total = 0;
 var subtotalTotal = 0
 
-
 var moneda = "UYU";
 
+
+//Funcion que calcula el costo de envio dependiendo el radioButton seleccionado
+function costoEnvio(envio) {
+  var totalDiv = document.getElementById("totalDiv")
+  var miTotal = 0
+  var costoEnvio = 0
+  //mostrar direccion de envio
+  document.getElementById("dirEnvio").style.display = "flex"
+
+  if (envio == 0) {
+    document.getElementById("dirEnvio").style.display = "none"
+    miTotal = subtotalTotal
+  }
+  if (envio == 1) {
+    costoEnvio = (subtotalTotal * 0.15)
+    miTotal = subtotalTotal + costoEnvio
+
+  }
+  if (envio == 2) {
+    costoEnvio = (subtotalTotal * 0.07)
+    miTotal = subtotalTotal + costoEnvio
+  }
+  if (envio == 3) {
+    costoEnvio = (subtotalTotal * 0.05)
+    miTotal = subtotalTotal + costoEnvio
+  }
+  totalDiv.innerHTML = `<strong>
+SUBTOTAL: `+ moneda + ` ` + subtotalTotal + `
+<br>
+ENVIO: `+ moneda + ` ` + costoEnvio + `
+<br>
+TOTAL: `+ moneda + ` ` + miTotal + `
+
+</strong>   `
+  //scrollear la pagina hasta la direccion de envio
+  location.href = "#dirEnvio"
+}
+
+//Esta funcion valida que no haya campos vacios al apretar el boton pagar
+//si pasa las validaciones se abre el modal con las formas de pago
 function btnPagar(e) {
   let defRadio = document.getElementById("defRadio");
   let calleEnvio = document.getElementById("calleEnvio");
@@ -17,6 +56,7 @@ function btnPagar(e) {
   for (let i = 0; i < cantidades.length; i++) {
     let cant = cantidades[i]
     if (cant.value == 0) {
+      //esto evita que se abra el modal
       e.stopPropagation()
       alert("indique una cantidad mayor a 0 en los productos seleccionados")
     }
@@ -27,193 +67,14 @@ function btnPagar(e) {
       alert("indique una direccion de envio")
     }
   }
-}
-
-//fUNCION QUE MUESTRA LA LISTA DE ARTICULOS DEL CARRITO
-function showCart(){   
-    //ELEMENTO list DEL HTML DONDE SE CARGAN LOS ARTICULOS
-    var cartListId = document.getElementById("cartListId")
-    //DECLARO UNA VARIABLE articles QUE CONTIENE LOS ARTICULOS DEL CARRITO
-   var articles =  currentCart.articles
-   //recorro los articulos
-   for (let i = 0; i <articles.length; i++) {
-     //declaro una variable CartProduct que corresponde al articulo del carrito por el que esta pasando el for
-   let CartProduct = articles[i];
-  //convierto el costo del articulo para que corresponda a la moneda indicada por la variable "moneda" declarada al principio
-  cost = convertir(CartProduct.unitCost, CartProduct.currency)
-  //cargo el contenido html de cada articulo en la lista
-  cartListId.innerHTML += `
-   <li class="cartList">
-   <!-- IMAGEN -->
-   <img class="cartListImg" src="`+CartProduct.src+`">
-   <!-- TITULO -->
-   <div>
-     <strong> 
-       <a href="">
-       `+CartProduct.name+`
-       </a>
-     </strong>
-   </div>
-   <!-- INFORMACION DEL PRODUCTO -->
- <div style="float:left;">
- `+CartProduct.currency+``+CartProduct.unitCost+`
-  <br>
-  Cantidad:
-  <input class="inputsCant" id="`+i+`" onInput="subtotal(`+i+`,`+cost+`)" type="number" placeholder="1" step="1" min="0" max="99" value=`+CartProduct.count+`>
-  <br>
-  <strong  id="`+i+`Sub" >
-  Subtotal: `+moneda+` `+CartProduct.count*cost+` 
-    </strong>
- </div>
- </li>     
-     `
-
-    //en el input que corresponde a la cantidad de articulos, creo un evento onInput
-    //con el cual al cambiar la cantidad se ejecuta la funcion "subtotal(i,costo)" que va a calcular el subtotal
-    //le doy un id unico a cada input para poder calcular el subtotal de cada articulo individualmente
-    //le doy id unico a la etiqueta strong donde se encuenta el subtotal, para de esta forma poder cambiar el contenido del subtotal de cada articulo
-    //la funcion subtotal se va a encargar de cambiar el contendio de la etiqueta strong que contiene el subtotal
-  //para obtener el subtotal  multiplico la cantidad por el costo unitario
- //guardo cada subtotal en un array con la posicion del index del for para luego poder hacer la suma total
- subTotales[i] = CartProduct.count*cost
-
- }
- //ejecuto la funcion sumarTotal calcula la suma de todos los subtotales
- sumarTotal()
-}
-
-//FUNCION QUE CALCULA EL SUBTOTAL
-  //la funcion recibe como parametro "i" que corresponde al index del for en showCart() y el costo,
-  //la i me proporciona una id unica para cada articulo y de esta forma puedo puedo acceder a su informacion de manera
-  function subtotal(i,cost){
-    //declaro la variable count, que corresponde al valor del input que determina la "cantidad" que se muestra en el html
-    count = document.getElementById(i).value
-    //declaro una variable que contiene el subtotal
-    currentSubTotal = count*cost
-    // declaro una variable que corresponde a la etiqueta strong donde se encuentra el subtotal
-    sub = document.getElementById(i+"Sub")
-    //cambio el contenido de la etiqueta strong por el nuevo subtotal
-    sub.innerHTML =`  
-    Subtotal: `+moneda+` `+currentSubTotal+`
-    `
-    //cambio el valor del subtotal en la posicion donde se encontraba el anterior
-    subTotales[i]=currentSubTotal
-   //ejecuto la funcion que suma el total, para obtener el nuevo total con el nuevo subtotal
-    sumarTotal();
-}
-
-//FUNCION QUE SUMA LOS SUBTOTALES Y MUESTRA EL TOTAL EN EL HTML
-function sumarTotal(){
-  //declaro una variable que contiene el div previamente preparado en el html para contener el total
-  var subtotalDiv = document.getElementById("subTotalDiv")
-  //inicializo el total en 0
-  var total = 0
-  //recorro el array donde tengo los subtotales
-  total = subTotales.reduce(getSum);
-  function getSum (total2,num){
-    return total2 + num;
+  if(subtotalTotal == 0){
+    e.stopPropagation()
+    alert("EL CARRITO ESTA VACIO")
   }
-
-  subtotalTotal = total
-
- //TOTAL POR DEFECTO
-  var totalDiv = document.getElementById("totalDiv")
-  totalDiv.innerHTML = `<strong>
-TOTAL: `+moneda+` `+subtotalTotal+`
-</strong>   `
-
-document.getElementById("defRadio").checked = true;
-document.getElementById("dirEnvio").style.display="none"
-
-}
-function costoEnvio(envio){
-  var totalDiv = document.getElementById("totalDiv")
-  var  miTotal = 0
-  var costoEnvio = 0
-//mostrar direccion de envio
- 
-  document.getElementById("dirEnvio").style.display="flex"
- 
-  if (envio == 0){
-    document.getElementById("dirEnvio").style.display="none"
-    miTotal = subtotalTotal
-    }
-if (envio == 1){
-costoEnvio = (subtotalTotal * 0.15)
-miTotal = subtotalTotal + costoEnvio
-
-}
-if (envio == 2){
-  costoEnvio =  (subtotalTotal * 0.07)
-  miTotal = subtotalTotal + costoEnvio
-  }
-  if (envio == 3){
-    costoEnvio =  (subtotalTotal * 0.05)
-    miTotal = subtotalTotal + costoEnvio
-    }
-totalDiv.innerHTML = `<strong>
-SUBTOTAL: `+moneda+` `+subtotalTotal+`
-<br>
-ENVIO: `+moneda+` `+costoEnvio+`
-<br>
-TOTAL: `+moneda+` `+miTotal+`
-
-</strong>   `
-//scrollear la pagina hasta la direccion de envio
-location.href = "#dirEnvio"
 }
 
-//FUNCIONES PARA CAMBIAR DE MONEDA, CADA UNA CORRESPONDE A UN BOTON PREVIAMENTE CREADO EN EL HTML
-    
-function pasarDolar(){
-  //limpio el contenido de la lista
-  var cartListId = document.getElementById("cartListId")
-  cartListId.innerHTML = ""
-  //cambio el valor de la moneda
-  moneda = "USD"
-  //vuelvo a cargar la lista
-  showCart();
-
-}
-function pasarPeso(){
-  var cartListId = document.getElementById("cartListId")
-  cartListId.innerHTML = ""
-  moneda = "UYU"
-  showCart();
-
-}
-
-function payBank(){
-  let paymentDiv = document.getElementById("payment")
-  paymentDiv.innerHTML = ``
-  paymentDiv.innerHTML = ` <div class="row mt-3">
-  <form>
-    <div class="form-row">
-    <div class="form-group "> <label for="Select Your Bank">
-    <h6>Select your Bank</h6>
-</label> <select class="form-control" id="selectBank" required>
-    <option value="" selected disabled>--Please select your Bank--</option>
-    <option>Banco Itaú Uruguay</option>
-    <option>Banco Santander</option>
-    <option>Banque Heritage.</option>
-    <option>BBVA</option>
-    <option>Citibank</option>
-    <option>HSBC Bank</option>
-    <option>Scotiabank Uruguay</option>
- 
-</select> </div>
-    </div>
-
-    <div class="form-row">
-    <div class="form-group col-md-8">
-      <label for="inputTitular">Numero de cuenta</label>
-      <input type="text" class="form-control" id="inputTitular" required>
-    </div>
-    <button  type="submit" onclick="confirmarPagoBank()" class="btn btn-primary">CONFIRMAR PAGO</button>
-  </form>
-</div>`
-}
-
+//funcion que carga en el modal el formulario de pago con tarjeta de credito
+//esta funcion se ejecuta al seleccionar la forma de pago con tarjeta credito/debito
 function payCredit(){
   let paymentDiv = document.getElementById("payment")
   paymentDiv.innerHTML = ``
@@ -249,43 +110,204 @@ function payCredit(){
 </div>`
 }
 
-function confirmarPago(){
 
+//funcion que carga en el modal el formulario de pago con cuenta bancaria
+//esta funcion se ejecuta al seleccionar la forma de pago con cuenta bancaria
+function payBank(){
+  let paymentDiv = document.getElementById("payment")
+  paymentDiv.innerHTML = ``
+  paymentDiv.innerHTML = ` <div class="row mt-3">
+  <form>
+    <div class="form-row">
+    <div class="form-group "> <label for="Select Your Bank">
+    <h6>Select your Bank</h6>
+</label> <select class="form-control" id="selectBank" required>
+    <option value="" selected disabled>--Please select your Bank--</option>
+    <option>Banco Itaú Uruguay</option>
+    <option>Banco Santander</option>
+    <option>Banque Heritage.</option>
+    <option>BBVA</option>
+    <option>Citibank</option>
+    <option>HSBC Bank</option>
+    <option>Scotiabank Uruguay</option>
+       </select> </div>
+    </div>
+    <div class="form-row">
+    <div class="form-group col-md-8">
+      <label for="inputTitular">Numero de cuenta</label>
+      <input type="text" class="form-control" id="inputTitular" required>
+    </div>
+    <button  type="submit" onclick="confirmarPagoBank()" class="btn btn-primary">CONFIRMAR PAGO</button>
+  </form>
+</div>`
+}
+
+
+
+//FUNCION PARA ELIMINAR UN ITEM DE LA LISTA
+function eliminarItem(i) {
+  //pregunto al usuario si esta seguro
+  var option = confirm("¿Esta seguro que deseaz eliminar el articulo?");
+  //si option es true elimino el articulo, sino no hace nada
+ if(option){
+  //le doy valor 0 al articulo en el array de los subtotales, para que no cuente su precio en el total
+  subTotales[i] = 0
+  // le doy display none al item de la lista para hacerlo desaparecer
+  document.getElementById(i + "liCart").style.display = "none";
+  //Ejecuto la funcioon que suma el total para actualizar el total
+  sumarTotal()
+ }
+}
+
+//funcion que valida q  ue no haya campos vacios en los datos de la cuenta bancaria y ademas si no los hay da un mensaje de transaccion exitosa
+
+function confirmarPagoBank() {
+  let exampleModal = document.getElementById("exampleModal")
+  let selectBank = document.getElementById("selectBank").value
+  let inputTitular = document.getElementById("inputTitular").value
+  if (selectBank != "" && inputTitular.length != 0) {
+    exampleModal.innerHTML =
+      `<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">PAGO ACEPTADO</h4>
+  <p>Su pago se completo con exito</p>
+  <hr>
+  <p class="mb-0">Gracias por su compra </p>
+</div>`
+  }
+}
+//funcion que valida q  ue no haya campos vacios en los datos de la cuenta bancaria y ademas si no los hay da un mensaje de transaccion exitosa
+function confirmarPago() {
   let exampleModal = document.getElementById("exampleModal")
   let inputTitular = document.getElementById("inputTitular").value
   let inputCVV = document.getElementById("inputCVV").value
   let inputCardNumber = document.getElementById("inputCardNumber").value
   let vencimientoM = document.getElementById("vencimientoM").value
   let vencimientoY = document.getElementById("vencimientoY").value
-  if(inputTitular.length != 0 && inputCVV.length != 0 && inputCardNumber.length != 0 && vencimientoM.length != 0 && vencimientoY.length != 0 ){
-  exampleModal.innerHTML =
-  `<div class="alert alert-success" role="alert">
+  if (inputTitular.length != 0 && inputCVV.length != 0 && inputCardNumber.length != 0 && vencimientoM.length != 0 && vencimientoY.length != 0) {
+    exampleModal.innerHTML =
+      `<div class="alert alert-success" role="alert">
   <h4 class="alert-heading">PAGO ACEPTADO</h4>
   <p>Su pago se completo con exito</p>
   <hr>
   <p class="mb-0">Gracias por su compra </p>
 </div>`
   }
-
-
 }
 
-function confirmarPagoBank(){
-  let exampleModal = document.getElementById("exampleModal")
-  let selectBank = document.getElementById("selectBank").value
-  let inputTitular = document.getElementById("inputTitular").value
- if(selectBank != "" && inputTitular.length != 0){
 
-  exampleModal.innerHTML =
-  `<div class="alert alert-success" role="alert">
-  <h4 class="alert-heading">PAGO ACEPTADO</h4>
-  <p>Su pago se completo con exito</p>
-  <hr>
-  <p class="mb-0">Gracias por su compra </p>
-</div>`
+//fUNCION QUE MUESTRA LA LISTA DE ARTICULOS DEL CARRITO
+function showCart() {
+  //ELEMENTO list DEL HTML DONDE SE CARGAN LOS ARTICULOS
+  var cartListId = document.getElementById("cartListId")
+  //DECLARO UNA VARIABLE articles QUE CONTIENE LOS ARTICULOS DEL CARRITO
+  var articles = currentCart.articles
+  //recorro los articulos
+  for (let i = 0; i < articles.length; i++) {
+    //declaro una variable CartProduct que corresponde al articulo del carrito por el que esta pasando el for
+    let CartProduct = articles[i];
+    //convierto el costo del articulo para que corresponda a la moneda indicada por la variable "moneda" declarada al principio
+    cost = convertir(CartProduct.unitCost, CartProduct.currency)
+    //cargo el contenido html de cada articulo en la lista
+    cartListId.innerHTML += `
+   <li id="`+ i + `liCart" class="cartList">
+   <!-- IMAGEN -->
+   <img class="cartListImg" src="`+ CartProduct.src + `">
+   <!-- TITULO -->
+   <div>
+     <strong> 
+       <a href="">
+       `+ CartProduct.name + `
+    </a>
+       <button onclick="eliminarItem(`+ i + `)" class="ml-2 badge badge-danger">Eliminar</button>
+     </strong>
+   </div>
+   <!-- INFORMACION DEL PRODUCTO -->
+ <div style="float:left;">
+ `+ CartProduct.currency + `` + CartProduct.unitCost + `
+  <br>
+  Cantidad:
+  <input class="inputsCant" id="`+ i + `" onInput="subtotal(` + i + `,` + cost + `)" type="number" placeholder="1" step="1" min="0" max="99" value=` + CartProduct.count + `>
+  <br>
+  <strong  id="`+ i + `Sub" >
+  Subtotal: `+ moneda + ` ` + CartProduct.count * cost + ` 
+    </strong>
+
+ </div>
+ </li>     
+     `
+
+    //en el input que corresponde a la cantidad de articulos, creo un evento onInput
+    //con el cual al cambiar la cantidad se ejecuta la funcion "subtotal(i,costo)" que va a calcular el subtotal
+    //le doy un id unico a cada input para poder calcular el subtotal de cada articulo individualmente
+    //le doy id unico a la etiqueta strong donde se encuenta el subtotal, para de esta forma poder cambiar el contenido del subtotal de cada articulo
+    //la funcion subtotal se va a encargar de cambiar el contendio de la etiqueta strong que contiene el subtotal
+    //para obtener el subtotal  multiplico la cantidad por el costo unitario
+    //guardo cada subtotal en un array con la posicion del index del for para luego poder hacer la suma total
+    subTotales[i] = CartProduct.count * cost
+
   }
-
+  //ejecuto la funcion sumarTotal calcula la suma de todos los subtotales
+  sumarTotal()
 }
+
+  //FUNCION QUE CALCULA EL SUBTOTAL
+  //la funcion recibe como parametro "i" que corresponde al index del for en showCart() y el costo,
+  //la i me proporciona una id unica para cada articulo y de esta forma puedo puedo acceder a su informacion de manera
+function subtotal(i,cost){
+    //declaro la variable count, que corresponde al valor del input que determina la "cantidad" que se muestra en el html
+    count = document.getElementById(i).value
+    //declaro una variable que contiene el subtotal
+    currentSubTotal = count*cost
+    // declaro una variable que corresponde a la etiqueta strong donde se encuentra el subtotal
+    sub = document.getElementById(i+"Sub")
+    //cambio el contenido de la etiqueta strong por el nuevo subtotal
+    sub.innerHTML =`  
+    Subtotal: `+moneda+` `+currentSubTotal+`
+    `
+    //cambio el valor del subtotal en la posicion donde se encontraba el anterior
+    subTotales[i]=currentSubTotal
+   //ejecuto la funcion que suma el total, para obtener el nuevo total con el nuevo subtotal
+    sumarTotal();
+}
+
+  //FUNCION QUE SUMA LOS SUBTOTALES Y MUESTRA EL TOTAL EN EL HTML
+function sumarTotal(){
+  //declaro una variable que contiene el div previamente preparado en el html para contener el total
+   //inicializo el total en 0
+  var total = 0
+  //recorro el array donde tengo los subtotales
+  total = subTotales.reduce(getSum);
+  function getSum (total2,num){
+    return total2 + num;
+  }
+  subtotalTotal = total
+ //TOTAL POR DEFECTO
+  var totalDiv = document.getElementById("totalDiv")
+  totalDiv.innerHTML = `<strong>
+  TOTAL: `+moneda+` `+subtotalTotal+`
+  </strong>   `
+  document.getElementById("defRadio").checked = true;
+  document.getElementById("dirEnvio").style.display="none"
+}
+
+
+  //FUNCIONES PARA CAMBIAR DE MONEDA, CADA UNA CORRESPONDE A UN BOTON PREVIAMENTE CREADO EN EL HTML
+function pasarDolar(){
+  //limpio el contenido de la lista
+  var cartListId = document.getElementById("cartListId")
+  cartListId.innerHTML = ""
+  //cambio el valor de la moneda
+  moneda = "USD"
+  //vuelvo a cargar la lista
+  showCart();
+}
+function pasarPeso(){
+  var cartListId = document.getElementById("cartListId")
+  cartListId.innerHTML = ""
+  moneda = "UYU"
+  showCart();
+}
+
 
 //FUNCION QUE CONVIERTE EL COSTO A LA MONEDA INDICADA EN LA VARIABLE moneda
 // se pasa por parametro el costo y el currency actual y
